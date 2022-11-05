5d227d4a47b12926a906c70ed052f45f65453f8e
Gantzhorn <andersgantzhorn@gmail.com>
2022-11-05 14:47:30 +0100

    online learning works

--- SGD_1.R
+++ SGD_1.R
@@ -6,22 +6,44 @@ decay_scheduler <- function(gamma1 = 1, a = 1, K = 1, gamma2, n1) {
   function(n) b / (K + n^a)
 }
 
-rate <- decay_scheduler(gamma1 = 0.0004, K = 100)
+rate <- decay_scheduler(gamma1 = 0.5, K = 100)
 
-beta <- vector("list", N)
+omega <- 4
 
-initpar <- c(0.5,0.3, 0.1, 1.5)
+sigma <- 0.1
 
-beta[[1]] <- initpar
-for (i in 2:10){
-  print(i)
-  print(glue::glue("alpha: ", beta[[i-1]][1]))
-  print(glue::glue("beta: ", beta[[i-1]][2]))
-  print(glue::glue("gamma: ", beta[[i-1]][3]))
-  print(glue::glue("rho: ", beta[[i-1]][4]))
-  beta[[i]] <- beta[[i-1]] - rate(i) * grad(X[i-1], beta[[i-1]][1],
-                                            beta[[i-1]][2],
-                                            beta[[i-1]][3],
-                                            beta[[i-1]][4])
+max_iter <- 5000
+
+par <- numeric(max_iter*4)
+
+dim(par) <- c(max_iter, 4)
+
+error <- numeric(max_iter)
+
+initpar <- c(alpha0+runif(1,-1,1), beta0+runif(1,-1,1), gamma0+runif(1,-1,1), rho0+runif(1,-1,1))
+
+par[1, ] <- initpar
+
+for (i in 2:max_iter){
+  print(glue::glue(i, ": "))
+  # Simulate online data 
+  X <- rnorm(1, mean = 0, sd = omega)
+  
+  epsilon <- rnorm(1, mean = 0, sd = sigma) 
+  
+  Y <- densY(X, alpha0, beta0, gamma0, rho0) + epsilon
+  
+  par[i, ] <- par[(i-1), ] - rate(i) * grad_calc(X, Y, par[(i-1), 1],
+                                                 par[(i-1), 2],
+                                                 par[(i-1), 3],
+                                                 par[(i-1), 4])
 }
+
+par
+
+ggplot(tibble(x = seq_along(par[, 1]), y = par[, 1]), aes(x = x, y = y)) + geom_line() + geom_hline(yintercept = alpha0, col = "red")
+ggplot(tibble(x = seq_along(par[, 1]), y = par[, 2]), aes(x = x, y = y)) + geom_line() + geom_hline(yintercept = beta0, col = "red")
+ggplot(tibble(x = seq_along(par[, 1]), y = par[, 3]), aes(x = x, y = y)) + geom_line() + geom_hline(yintercept = gamma0, col = "red")
+ggplot(tibble(x = seq_along(par[, 1]), y = par[, 4]), aes(x = x, y = y)) + geom_line() + geom_hline(yintercept = rho0, col = "red")
+  
 
--- scriptR.R
+++ scriptR.R
@@ -6,74 +6,60 @@ library(Rcpp)
 library(splines)
 library(xtable)
 library(Matrix)
+library(numDeriv)
 
 ## Key-functions ##
 
 #Density
 densY <- function(x, alpha, beta, gamma, rho){
-  gamma + (rho-gamma)/(1+exp(beta*log(x)-alpha)) #Note that x is log-normal
+  gamma + (rho-gamma)/(1+exp(beta*x-alpha)) 
 }
 
+# Nonlinear least squares
+
 #Gradient
-grad <- function(x, alpha, beta, gamma, rho){
+grad_calc <- function(x, y, alpha, beta, gamma, rho){
   N <- length(x)
-  grad_alpha <- 2/N*sum(Y-gamma+(gamma-rho)/(1+exp(beta*log(x)-alpha))*
-                          ((gamma-rho)*exp(beta*log(x)-alpha))/(exp(beta*log(x)-alpha)+1)^2)
+  grad_alpha <- 2/N*sum(
+                         (y-gamma+(gamma-rho)/(1+exp(beta*x-alpha)))*
+                         ((gamma-rho)*exp(beta*x-alpha))/((exp(beta*x-alpha)+1)^2)
+                       )
   
-  grad_beta <- -2/N*sum(Y-gamma+(gamma-rho)/(1+exp(beta*log(x)-alpha))*
-                         ((gamma-rho)*log(x)*exp(beta*log(x)-alpha))/(exp(beta*log(x)-alpha)+1)^2)
+  grad_beta <- -2/N*sum(
+                         (y-gamma+(gamma-rho)/(1+exp(beta*x-alpha)))*
+                         ((gamma-rho)*x*exp(beta*x-alpha))/((exp(beta*x-alpha)+1)^2)
+                       )
   
-  grad_gamma <- 2/N*sum(Y-gamma+(gamma-rho)/(1+exp(beta*log(x)-alpha))*
-                           (1/(1+exp(beta*log(x)-alpha))-1))
+  grad_gamma <- 2/N*sum(
+                         (y-gamma+(gamma-rho)/(1+exp(beta*x-alpha)))*
+                         (1/(1+exp(beta*x-alpha))-1)
+                       )
   
-  grad_rho <- -2/N*sum(Y-gamma+(gamma-rho)/(1+exp(beta*log(x)-alpha))*
-                         1/(1+exp(beta*log(x)-alpha)))
+  grad_rho <- -2/N*sum(
+                         (y-gamma+(gamma-rho)/(1+exp(beta*x-alpha)))*
+                         (1/(1+exp(beta*x-alpha)))
+                       )
   c(grad_alpha, grad_beta, grad_gamma, grad_rho)
 }
 
-
-## Simuliation ##
-
-#Number of observations
-N <- 5000
-
-sigma <- 0.2
-epsilon <- rnorm(N, mean = 0, sd = sigma)
-
-omega <- 0.5
-
-X <- rlnorm(N, mean = 0, sd = omega)
-
 #True parameters
-alpha0 <- 0.5
-beta0 <- 0.3
-gamma0 <- 0.1
-rho0 <- 1.5
+alpha0 <- 1
+beta0 <- 2
+gamma0 <- 5
+rho0 <- 2
 
-# Y-values
-Y <- densY(X, alpha0, beta0, gamma0, rho0) + epsilon
 
-grad(X, alpha0, beta0, gamma0, rho0)
 
-#Horsedata
-horse_data <- readr::read_csv("4_Horses.csv", col_types = cols(dead = col_logical()))
-horse_missing <- horse_data %>%
-  mutate(missing = ifelse(is.na(Temperature) == TRUE, TRUE,FALSE)) %>%
-  group_by(dead,missing) %>%
-  summarise(n = n())
-
-dummy <- seq(-10,10, by = 0.5)
-
-alpha_col <- numeric(41)
-beta_col <- numeric(41)
-gamma_col <- numeric(41)
-rho_col <- numeric(41)
-
-for (i in seq_along(dummy)){
-  alpha_col[i] <- grad(X, dummy[i], beta0, gamma0, rho0)[1]
-  beta_col[i] <- grad(X, alpha0, dummy[i], gamma0, rho0)[2]
-  gamma_col[i] <- grad(X, alpha0, beta0, dummy[i], rho0)[3]
-  rho_col[i] <- grad(X, alpha0, beta0, gamma0, dummy[i])[4]
+# Grad-test
+test_points <- c()
+obj_func <- function(inp){
+  1/length(test_points[1])*(sum((test_points[2]-densY(test_points[1], inp[1], inp[2], inp[3], inp[4]))^2))
 }
-tester <- tibble(inp = dummy, alpha = alpha_col, beta = beta_col, gamma = gamma_col, rho = rho_col)
+
+
+
+grad_calc(test_points[1], test_points[2], alpha0, beta0, gamma0, rho0)
+
+numDeriv::grad(obj_func, x = c(alpha0, beta0, gamma0, rho0))
+
 
